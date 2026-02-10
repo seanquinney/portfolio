029ae90fed53765d9718544cdb27fe36af3f4447
Sean Quinney <sean97quinney@gmail.com>
2026-02-10 21:16:54 +0000

    updating main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './style.css'
+import './style.css'                 // Vite handles CSS bundling
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
